9e48235363f9159987b53b053b563028248d3eb5
raphatsa-bit <rphatsa.p@ku.th>
2026-07-26 14:05:14 +0700

    Add files via upload

--- BoundedStack.java
+++ BoundedStack.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoundedStack {
 
+    private final List<String> elements;
+    private final int nobdy;
+    
+    /**
+     * 
+     * @param nobdy
+     */
+    public BoundedStack(int nobdy){
+
+        this.elements = new ArrayList<>();
+        this.nobdy = nobdy;
+
+    }
+    
+    /**
+     * 
+     * @param r
+     */
+    public void push(String r){
+
+
+    }
     
 }
--- TestRunner.java
+++ TestRunner.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestRunner {
+
+    private static int pass_test = 0;
+    private static int failed_test = 0;
+
+    /** 
+     * 
+     *
+     */
+    private static void check(String name, boolean condition) {
+        if (condition) {
+            pass_test++;
+            System.out.println("[PASS] " + name);
+        } else {
+            failed_test++;
+            System.out.println("[FAIL] " + name);
+        }
+    }
     
 }
